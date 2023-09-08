8e7baea9beb326d7ea91ac0cdb393efee91f8669
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-09-08 13:18:29 +0200

    init login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home-page/home-page.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageModule } from './pages/login-page/login-page.module';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login-page/login-page.module').then(m=>m.LoginPageModule)
   },
 ];
 
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl ('', [Validators.required, Validators.email]),
+    password: new FormControl ('', [Validators.required])
+  })
+  
+  submit(){
+    
+  }
+}

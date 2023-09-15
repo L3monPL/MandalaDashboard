37a524af432fc10d9d63665ccb26d3ea0e7ee580
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-09-15 10:08:35 +0200

    change realization hover and footer and hide message in dashboard

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  openMap(){
+    window.open(`https://www.google.com/maps/place/Wakacyjna+19%2F2,+60-185+Sk%C3%B3rzewo/@52.40076,16.785775,17z/data=!3m1!4b1!4m5!3m4!1s0x470445d1098716c1:0x6fe452cc4c29acf5!8m2!3d52.40076!4d16.785775?entry=ttu`)
+  }
 }
